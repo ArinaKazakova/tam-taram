260020d8b4fde1762daf63c2f6e843043500fdcd
ArinaKazakova <arinakazakovak@gmail.com>
2019-03-14 12:57:31 +0300

    edit drop-down menu

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,10 +1,8 @@
 
 
 	//Toggle menu
-    $('.header__nav-list').removeClass('header__nav-list_open');
-
     $('.header__nav-toggle').on('click' , function(){
-        $('.header__nav-list').toggleClass('header__nav-list_open');
+        $('.header__nav-list').slideToggle(500);
     });
 
 	//Search field
@@ -12,13 +10,12 @@
     $('.search__field').removeClass('search__field_open');
     $('.search__button-toggle').on('click' , function(){
         $('.search__field').toggleClass('search__field_open');
-    });
+    })
 
 	//Vide-block/ Height setting
 
-
-    $('.container__button').on('click' , function(){
-        const contentHeight = $(this).prev('.video-slider').height();
+   /* $('.container__button').on('click' , function(){
+        const contentHeight = $(this).prev('.slick-list').height();
         const itemHeight =  $('.video-slider__item').height() + 30;
         const content = $(this).prev('.video-slider').children();
         const fullContentHeight = itemHeight * content.children().length ;
@@ -29,11 +26,9 @@
         } else {
             $(this).prev('.video-slider').css('height', fullContentHeight);
         }
-    });
+    });*/
 
-
-
-    $(document).ready(function() {
+    function slickSlider(){
         $('.video-slider').slick({
             infinite:false,
             speed: 300,
@@ -45,7 +40,7 @@
             nextArrow: ' <button id="next" class="container__next"></button>',
             responsive: [
                 {
-                    breakpoint: 1111,
+                    breakpoint: 1024,
                     settings: {
                         slidesToShow: 2,
                         slidesToScroll: 1
@@ -54,7 +49,7 @@
                 },
 
                 {
-                    breakpoint: 480,
+                    breakpoint: 640,
                     settings: {
                         slidesToShow: 1,
                         slidesToScroll: 1
@@ -63,5 +58,9 @@
                 }
             ]
         });
+    }
 
-    });
+    if(screen.width > 640){
+        console.log(screen.width);
+        slickSlider();
+    }
